ee8d99cfb2f67949143ee2154316b98bd63327e9
Tullu <imratan05@gmail.com>
2019-12-25 17:59:28 +0530

    this is first commit of project 10

---
+++ javademoGit10/src/p1/Test10.java
@@ -0,0 +1,17 @@
+package p1;
+
+public class Test10 {
+
+	void fun1(){
+		System.out.println("inside fun1 of Test10");
+	}
+	
+	public static void main(String[] args) {
+
+		new Test10().fun1();
+		
+		System.out.println("End of Main...");
+
+	}
+
+}
